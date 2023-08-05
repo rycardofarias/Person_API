a08a20c68d7db9d5e7aa94c9a5be02bef6a7f47a
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-05 18:51:56 -0300

    Validating Swagger UI Page Generation with Testcontainers and Rest Assured

---
+++ src/test/java/com/example/demo/configs/TestConfigs.java
@@ -0,0 +1,7 @@
+package com.example.demo.configs;
+
+
+public class TestConfigs {
+
+	public static final int SERVER_PORT = 8888;
+}
---
+++ src/test/java/com/example/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.example.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.example.demo.startup;
+import com.example.demo.configs.TestConfigs;
+import com.example.demo.integrationtests.testcontainers.AbstractIntegrationTest;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest{
+
+	@Test
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(TestConfigs.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
+	}
+
+}
+
