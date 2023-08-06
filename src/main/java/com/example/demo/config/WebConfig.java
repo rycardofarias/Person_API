da40c48cdb3603962f64610efb2dc7da7f17de57
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-06 15:50:59 -0300

    Enabling CORS Globally

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,12 +1,28 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+	
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		var allowedOrigins = corsOriginPatterns.split(",");
+		registry.addMapping("/**")
+			.allowedMethods("*")
+			.allowedOrigins(allowedOrigins)
+			.allowCredentials(true);
+	}
+
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
