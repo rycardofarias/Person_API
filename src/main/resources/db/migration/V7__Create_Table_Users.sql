4ee810a2aeec8e3b356bb49aa14efbb9a48aa1b6
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-10 00:06:13 -0300

    Adding migration sqls

---
+++ src/main/resources/db/migration/V7__Create_Table_Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE IF NOT EXISTS users (
+  id bigserial PRIMARY KEY,
+  user_name varchar(255),
+  full_name varchar(255),
+  password varchar(255),
+  account_non_expired boolean,
+  account_non_locked boolean,
+  credentials_non_expired boolean,
+  enabled boolean
+);
