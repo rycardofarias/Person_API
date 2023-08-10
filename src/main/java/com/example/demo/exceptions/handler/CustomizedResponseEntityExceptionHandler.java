1ed4962628f5b3af697fe2e260dfcd708fd5a3c4
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-10 02:19:07 -0300

    Creating Custom Exceptions

--- src/main/java/com/example/demo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.demo.exceptions.ExceptionResponse;
+import com.example.demo.exceptions.InvalidJwtAuthenticationException;
 import com.example.demo.exceptions.RequiredObjectsIsNullException;
 import com.example.demo.exceptions.ResourceNotFoundException;
 
@@ -48,5 +49,16 @@ public class CustomizedResponseEntityExceptionHandler  extends ResponseEntityExc
 				request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex,
+			WebRequest request){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 }
