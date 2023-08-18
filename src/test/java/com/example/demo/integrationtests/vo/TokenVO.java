729412a5d5a82d48dad848f1002b935e7bc79a0f
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-18 01:13:27 -0300

    Test Token VO

---
+++ src/test/java/com/example/demo/integrationtests/vo/TokenVO.java
@@ -0,0 +1,80 @@
+package com.example.demo.integrationtests.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class TokenVO implements Serializable{
+
+	private static final long serialVersionUID = 1;
+	
+	private String username;
+	private boolean authenticated;
+	private Date created;
+	private Date expiration;
+	private String accessToken;
+	private String refreshToken;
+	
+	public TokenVO() {}
+	
+	public TokenVO(String username, boolean authenticated, 
+			Date created, Date expiration, String accessToken,
+			String refreshToken) {
+		
+		this.username = username;
+		this.authenticated = authenticated;
+		this.created = created;
+		this.expiration = expiration;
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public boolean isAuthenticated() {
+		return authenticated;
+	}
+
+	public void setAuthenticated(boolean authenticated) {
+		this.authenticated = authenticated;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Date getExpiration() {
+		return expiration;
+	}
+
+	public void setExpiration(Date expiration) {
+		this.expiration = expiration;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}	
+	
+}
