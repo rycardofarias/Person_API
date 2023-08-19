5932fd50ecec4eb276be5ab2913f0f81160292c6
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-19 18:02:22 -0300

    Configuring the PersonRepository to add the disabled column in the Person table

--- src/main/java/com/example/demo/repositories/PersonRepository.java
+++ src/main/java/com/example/demo/repositories/PersonRepository.java
@@ -1,11 +1,19 @@
 package com.example.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Person;
+import com.example.demo.model.User;
 
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+	
+	@Modifying
+	@Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
+	void disablePerson(@Param("id") Long id);
 }
