1e052b9e9fcd6eec32b846b23cd3cceaa1248b15
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-20 14:44:17 -0300

    Delete people table and create people table with 1000 records to do pagination

---
+++ src/main/resources/db/migration/V12__Drop_Create_Person.sql
@@ -0,0 +1,11 @@
+DROP TABLE person;
+
+CREATE TABLE IF NOT EXISTS person (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(80) NOT NULL,
+  last_name VARCHAR(80) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  gender VARCHAR(6) NOT NULL,
+  enabled BOOLEAN NOT NULL 
+);
+
