2c6ca8f6365a962d5677ce34d2efb904c06c2c18
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-20 15:44:14 -0300

    Implementing Pagination with Pageable and Query Params

--- src/main/java/com/example/demo/services/PersonServices.java
+++ src/main/java/com/example/demo/services/PersonServices.java
@@ -1,11 +1,13 @@
 package com.example.demo.services;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.controllers.PersonController;
@@ -26,18 +28,19 @@ public class PersonServices {
 	@Autowired
 	PersonRepository repository;
 	
-	public List<PersonVO> findAll() {
+	public Page<PersonVO> findAll(Pageable pageable) {
 		
 		logger.info("Finding all persons!");
 		
-		var persons = DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
-	
-		persons
-			.stream()
-			.forEach(p -> p.add(
-					linkTo(methodOn(
-							PersonController.class).findById(p.getKey())).withSelfRel()));
-		return persons;
+		var personPage = repository.findAll(pageable);
+		
+		var personVOsPage =  personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+		
+		personVOsPage.map(p -> p.add(
+				linkTo(methodOn(
+						PersonController.class).findById(p.getKey())).withSelfRel()));
+		
+		return personVOsPage;
 	}
 	
 	public PersonVO findById(Long id) {
