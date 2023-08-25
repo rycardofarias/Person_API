ce9f6be991db8c8cfc100f44276f400fd8be9fcf
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-25 01:30:18 -0300

    Created PersonEmbeddedVO and WrapperPersonVO

---
+++ src/test/java/com/example/demo/integrationtests/vo/wrapper/PersonEmbeddedVO.java
@@ -0,0 +1,47 @@
+package com.example.demo.integrationtests.vo.wrapper;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+import com.example.demo.integrationtests.vo.PersonVO;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PersonEmbeddedVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonProperty("personVOList")
+	private List<PersonVO> persons;
+
+	public PersonEmbeddedVO() {
+		super();
+	}
+
+	public List<PersonVO> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(List<PersonVO> persons) {
+		this.persons = persons;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(persons);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonEmbeddedVO other = (PersonEmbeddedVO) obj;
+		return Objects.equals(persons, other.persons);
+	}
+	
+}
+
---
+++ src/test/java/com/example/demo/integrationtests/vo/wrapper/WrapperPersonVO.java
@@ -0,0 +1,44 @@
+package com.example.demo.integrationtests.vo.wrapper;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class WrapperPersonVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonProperty("_embedded")
+	private PersonEmbeddedVO embedded;
+
+	public WrapperPersonVO() {
+	}
+
+	public PersonEmbeddedVO getEmbedded() {
+		return embedded;
+	}
+
+	public void setEmbedded(PersonEmbeddedVO embedded) {
+		this.embedded = embedded;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(embedded);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WrapperPersonVO other = (WrapperPersonVO) obj;
+		return Objects.equals(embedded, other.embedded);
+	}
+	
+	
+}
