19568afef84ba993ff374afe3532da8248c2e617
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-08-26 12:32:52 -0300

    Adjusting PersonServicesTests testFindAll

--- src/test/java/com/example/demo/unitest/mockito/services/PersonServiceTest.java
+++ src/test/java/com/example/demo/unitest/mockito/services/PersonServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -18,6 +19,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 
 import com.example.demo.data.vo.v1.PersonVO;
 import com.example.demo.exceptions.RequiredObjectsIsNullException;
@@ -39,7 +48,10 @@ class PersonServicesTest {
 	
 	@Mock
 	PersonRepository repository;
-	
+
+    @Mock
+    private PagedResourcesAssembler<PersonVO> assembler;
+    
 	@BeforeEach
 	void setUpMocks() throws Exception {
 		input = new MockPerson();
@@ -155,53 +167,20 @@ class PersonServicesTest {
 		service.delete(1L);
 	}
 	
-	@Test
-	void testFindAll() {
-		List<Person> list = input.mockEntityList(); 
-		
-		when(repository.findAll()).thenReturn(list);
-		
-		var people = service.findAll();
-		
-		assertNotNull(people);
-		assertEquals(14, people.size());
-		
-		var personOne = people.get(1);
-		
-		assertNotNull(personOne);
-		assertNotNull(personOne.getKey());
-		assertNotNull(personOne.getLinks());
-		
-		assertTrue(personOne.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
-		assertEquals("Addres Test1", personOne.getAddress());
-		assertEquals("First Name Test1", personOne.getFirstName());
-		assertEquals("Last Name Test1", personOne.getLastName());
-		assertEquals("Female", personOne.getGender());
-		
-		var personFour = people.get(4);
-		
-		assertNotNull(personFour);
-		assertNotNull(personFour.getKey());
-		assertNotNull(personFour.getLinks());
-		
-		assertTrue(personFour.toString().contains("links: [</api/person/v1/4>;rel=\"self\"]"));
-		assertEquals("Addres Test4", personFour.getAddress());
-		assertEquals("First Name Test4", personFour.getFirstName());
-		assertEquals("Last Name Test4", personFour.getLastName());
-		assertEquals("Male", personFour.getGender());
-		
-		var personSeven = people.get(7);
-		
-		assertNotNull(personSeven);
-		assertNotNull(personSeven.getKey());
-		assertNotNull(personSeven.getLinks());
-		
-		assertTrue(personSeven.toString().contains("links: [</api/person/v1/7>;rel=\"self\"]"));
-		assertEquals("Addres Test7", personSeven.getAddress());
-		assertEquals("First Name Test7", personSeven.getFirstName());
-		assertEquals("Last Name Test7", personSeven.getLastName());
-		assertEquals("Female", personSeven.getGender());
+	 @Test
+    public void testFindAll() {
+
+		Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.ASC, "firstName"));
+	    
+	    List<Person> list = input.mockEntityList();
+
+	    Page<Person> page = new PageImpl<>(list, pageable, list.size());
+	    when(repository.findAll(eq(pageable))).thenReturn(page);
+
+	    PagedModel<EntityModel<PersonVO>> people = service.findAll(pageable);
+
+	    assertNotNull(people);
+	    assertEquals(list.size(), people.getMetadata().getSize());
 
 	}
-
 }
